efc08076e58d7fb54fac97a0a5622acf07807b69
”Pavlo <levkopavlo@gmail.com>
2024-08-13 00:10:19 +0300

    create crud for books

--- src/main/java/com/example/demotask/controller/BooksController.java
+++ src/main/java/com/example/demotask/controller/BooksController.java
@@ -6,19 +6,34 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 @AllArgsConstructor
 @RestController
-@RequestMapping("api/books")
+@RequestMapping("/api")
 
 public class BooksController {
     private final BooksService booksService;
-    @GetMapping
+    @GetMapping("/books")
     public List<Book> getAllBook(){
    return booksService.getAllBooks();
+    }
+    @GetMapping("/{id}")
+    public Optional<Book> getBookById(@PathVariable Long id){
+       return booksService.getById(id);
     }
     @PostMapping
     @RequestMapping("/book")
     public Long addBook(@RequestBody Book book){
         return booksService.addBook(book);
+    }
+    @PutMapping("/book/{id}")
+    public Book updateBook(@PathVariable Long id,
+                           @RequestBody Book book){
+      return booksService.updateBook(id, book);
+    }
+    @DeleteMapping("/book/{id}")
+    public void deleteBookById(@PathVariable Long id){
+        booksService.deleteBook(id);
     }
 }
--- src/main/java/com/example/demotask/service/BooksService.java
+++ src/main/java/com/example/demotask/service/BooksService.java
@@ -1,11 +1,14 @@
 package com.example.demotask.service;
 
 import com.example.demotask.entity.Book;
+import com.example.demotask.exception.NotFoundException;
 import com.example.demotask.repository.BooksRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -20,5 +23,24 @@ public class BooksService {
     public Long addBook(Book book) {
         Book savedBook = repository.save(book);
         return savedBook.getId();
+    }
+
+    public Optional<Book> getById(Long id) {
+      return repository.findById(id);
+    }
+
+    public void deleteBook(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Book updateBook(Long id, Book book) {
+        Book bookInDataBase = (repository.
+                findById(id).orElseThrow(() -> new NotFoundException("Book with id " + id + " not found!")));
+
+        bookInDataBase.setTitle(book.getTitle());
+        bookInDataBase.setAuthor(book.getAuthor());
+        bookInDataBase.setAmount(book.getAmount());
+
+      return repository.save(bookInDataBase);
     }
 }

930ab8790a5592bcf819d7a312bc652a6fb82aaf
”Pavlo <levkopavlo@gmail.com>
2024-08-12 23:13:25 +0300

    first commit

---
+++ src/main/java/com/example/demotask/entity/Book.java
@@ -0,0 +1,13 @@
+package com.example.demotask.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private int amount;
+}
---
+++ src/main/java/com/example/demotask/entity/Members.java
@@ -0,0 +1,4 @@
+package com.example.demotask.entity;
+
+public class Members {
+}
